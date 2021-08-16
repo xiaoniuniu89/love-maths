6ca8af976a68f178d2b27bfa488c3091201050ea
Daniel Callaghan <dan_cal89@hotmail.com>
2021-08-16 12:42:21 +0000

    add addition answer checker

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!")
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    runGame("addition")
+    runGame("addition");
 });
 
 /**
@@ -31,16 +31,40 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting`;
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
 
-function checkAnswer() {
+/**
+ * checks answer against first element in the returned calculateCorrectAnswer array
+ */
 
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert("Hey! You got it right! :D")
+    } else {
+        alert(`Awww .... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+    runGame(calculatedAnswer[1]);
 }
 
+/**
+ * Gets the operands and the operator directly from the dom and returns the correct answer
+ */
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplimented operator ${operator}`);
+        throw `Unimplimented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
